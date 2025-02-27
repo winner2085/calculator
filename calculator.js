1dc4bcde16df6c2ad9c2cdff3f5905a52fc5dfdc
Erica <ericac2700@tuckahoeschools.org>
2025-02-27 13:30:14 +0000

    UGH

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var textbox = document.querySelector("#userInput");
+    var textbox = document.querySelector("#result");
     var numButtons = document.querySelectorAll(".buttons");
     
     numButtons.forEach(function(btn) {
@@ -9,3 +9,33 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function solve() {
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide' ||
+
+  if(action === 'calculate') {
+    const firstValue = calculater.dataset.firstValue;
+    const operator = calculater.dataset.operator;
+    const secondValue = displayedNum;
+
+    display.textContent = calculate(firstValue, operator, secondValue);
+  }
+
+  const calculate = (n1, operator, n2) => {
+    let result = '';
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2);
+    }
+    return result;
+  }
+}
+
+solve ()

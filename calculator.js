6b76fa75fdeba1c630a48fb2f2c3fc919ac890eb
Erica <ericac2700@tuckahoeschools.org>
2025-02-28 04:26:58 +0000

    no

--- calculator.js
+++ calculator.js
@@ -2,40 +2,70 @@ document.addEventListener('DOMContentLoaded', function() {
     var textbox = document.querySelector("#result");
     var numButtons = document.querySelectorAll(".buttons");
     
+    var firstValue = '';
+    var secondValue = '';
+    var operator = '';
+    var isSecondValue = false;
+
+    //when the user clicks on a button
     numButtons.forEach(function(btn) {
       btn.addEventListener("click", function(e) {
-        textbox.value += this.value;
+        var value = this.value;
       });
     });
 });
 
+//clear calculator
+function clear() {
+  if(value === 'C') {
+    firstValue = '';
+    secondValue = '';
+    operator = '';
+    textbox.value = '';
+    isSecondValue = false;
+  } else if (value === "=") { 
+    if (firstValue && secondValue && operator) {
+      textbox.value = calculate(firstValue, operator, secondValue);
+      firstValue = textbox.value;
+      secondValue = '';
+      operator = '';
+      isSecondValue = false;
+    }
+  }
+}
+
 function solve() {
-  action === 'add' ||
-  action === 'subtract' ||
-  action === 'multiply' ||
-  action === 'divide' ||
-
-  if(action === 'calculate') {
-    const firstValue = calculater.dataset.firstValue;
-    const operator = calculater.dataset.operator;
-    const secondValue = displayedNum;
-
-    display.textContent = calculate(firstValue, operator, secondValue);
+    if (['+', '-', '*', '/'].includes(value)) {
+    operator = value;
+    firstValue = textbox.value;
+    textbox.value = '';
+    isSecondValue = true;
+    } else {
+    if (isSecondValue) {
+      secondValue += value;
+    }else {
+      firstValue += value;
+    }
+    textbox.value += value;
   }
 
   const calculate = (n1, operator, n2) => {
     let result = '';
-    if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2);
-    } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2);
-    } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2);
-    } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2);
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+
+    if (operator === '+') {
+      result = n1 + n2;
+    } else if (operator === '-') {
+      result = n1 - n2;
+    } else if (operator === '*') {
+      result = n1 * n2;
+    } else if (operator === '/') {
+      result = n1 / n2;
     }
     return result;
   }
 }
 
-solve ()
+solve()
+clear()
